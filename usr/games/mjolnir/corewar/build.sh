8c765076b7265173fa2e28a7717f15ad5628aa2c
Tuomo Petteri Ve'näläinen <tuomoster@gmail.com>
2020-11-30 16:11:06 +0200

    FUCK YOU GIT

--- usr/games/mjolnir/corewar/build.sh
+++ usr/games/mjolnir/corewar/build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-gcc -DCW_DEBUG -DCW_BIG_CORE -DTEST_CW -g -Wall -Wextra -Wundef -I.. -I../../../lib -O -o cw cw.c rc.c zeus.c x11.c ../../../lib/prng/randmt32.c -lX11 -lImlib2 -lSDL2 -lSDL2_image
+gcc -DCW_DEBUG -DCW_BIG_CORE -DTEST_CW -g -Wall -Wextra -Wundef -I.. -I../../../lib -O -o mars cw.c rc.c ../../../lib/prng/randmt32.c
+gcc -DZEUS -DZEUSX11 -DZEUSIMLIB2 -DCW_DEBUG -DCW_BIG_CORE -DTEST_CW -g -Wall -Wextra -Wundef -I.. -I../../../lib -O -o zeus cw.c rc.c ../../../lib/prng/randmt32.c zeus.c x11.c -lX11 -lImlib2 -lSDL2 -lSDL2_image
 
