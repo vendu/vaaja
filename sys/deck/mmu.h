12ebc95ca070e25274771d2513046c9805f49c5c
Tuomo Petteri Ve'näläinen <tuomoster@gmail.com>
2020-12-04 02:38:58 +0200

    ...

--- sys/deck/mmu.h
+++ sys/deck/mmu.h
@@ -5,9 +5,9 @@
 
 #include <stdint.h>
 
-#define DECK_PAGE_SIZE          4096
-#define DECK_PAGE_ADR_BITS      20
-#define DECK_PAGE_OFS_BITS      12
+#define DECK_PAGE_SIZE          1024
+#define DECK_PAGE_ADR_BITS      22
+#define DECK_PAGE_OFS_BITS      10
 
 /* flg-field */
 #define DECK_MTR_NT             0x00
@@ -17,13 +17,28 @@
 #define DECK_MTR_WB             0x04
 #define DECK_MTR_TYPE_BITS      3
 
-#define DECK_MTR_NPG_BITS       9
+#define DECK_PAGE_USER_EXEC     (1 << 0)
+#define DECK_PAGE_USER_WRITE    (1 << 1)
+#define DECK_PAGE_USER_READ     (1 << 2)
+#define DECK_PAGE_GROUP_EXEC    (1 << 3)
+#define DECK_PAGE_GROUP_WRITE   (1 << 4)
+#define DECK_PAGE_GROUP_READ    (1 << 5)
+#define DECK_PAGE_WORLD_EXEC    (1 << 6)
+#define DECK_PAGE_WORLD_WRITE   (1 << 7)
+#define DECK_PAGE_WORLD_READ    (1 << 8)
+#define DECK_PAGE_SHARE_PERM    (1 << 9)
+#define DECK_PAGE_WIRE_PERM     (1 << 10)
+#define DECK_PAGE_PERM_BITS     10
 
-/* the actual size of a region is npg + 1 pages; max is 512 pages = 2 MB */
+/* the actual size of a region is npg + 1 pages */
 struct deckmtr {
+    /* header */
     unsigned                    base    : DECK_PAGE_ADR_BITS;   // range address
-    unsigned                    npg     : DECK_MTR_NPG_BITS;    // max 512 pages
     unsigned                    type    : DECK_MTR_TYPE_BITS;   // cache policy
+    unsigned                    flg     : 7;                    // op flags
+    /* footer */
+    unsigned                    npg     : DECK_PAGE_ADR_BITS;   // # of pages
+    unsigned                    perm    : DECK_PAGE_PERM_BITS;  // permissions
 };
 
 #define DECK_PAGE_EXEC          (UINT32_C(1) << 0)  // page execution permission
--- sys/deck/sanity.h
+++ sys/deck/sanity.h
@@ -10,50 +10,68 @@
 /* check structure sizes in <deck/inst.h> */
 
 #if (DECK_OP_BITS + DECK_COND_BITS + 2 * DECK_GEN_REG_BITS              \
-     + DECK_REG_BITS + DECK_ADR_BITS + DECK_FOLD_BITS + 8 != DECK_INST_BITS)
-#   error sizeof(struct deckinst) != DECK_INST_BITS <deck/inst.h>
+     + DECK_REG_BITS + DECK_ADR_BITS + DECK_FOLD_BITS + 8               \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
 #if (DECK_OP_BITS + DECK_COND_BITS + DECK_GEN_REG_BITS + DECK_REG_BITS  \
-     + 7 + 9 != DECK_INST_BITS)
-#   error sizeof(struct deckcmpinst) != DECK_INST_BITS <deck/inst.h>
+     + 7 + 9                                                            \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckcmpinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
-#if (DECK_OP_BITS + DECK_OFS_BITS != DECK_INST_BITS)
-#   error sizeof(struct deckjmpinst) != DECK_INST_BITS <deck/inst.h>
+#if (DECK_OP_BITS + DECK_OFS_BITS                                       \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckjmpinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
-#if (DECK_OP_BITS + DECK_COND_BITS + DECK_BRA_BITS != DECK_INST_BITS)
-#   error sizeof(struct deckbrainst) != DECK_INST_BITS <deck/inst.h>
+#if (DECK_OP_BITS + DECK_COND_BITS + DECK_BRA_BITS                      \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckbrainst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
-#if (DECK_OP_BITS + DECK_COND_BITS + 7 + DECK_REG_BITS + 12 != DECK_INST_BITS)
-#   error sizeof(struct deckstkhinst) != DECK_INST_BITS <deck/inst.h>
+#if (DECK_OP_BITS + DECK_COND_BITS + 7 + DECK_REG_BITS + 12             \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckstkhinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
-#if (DECK_OP_BITS + DECK_COND_BITS + 7 + DECK_INT_REGS != DECK_INST_BITS)
-#   error sizeof(struct deckmapinst) != DECK_INST_BITS <deck/inst.h>
+#if (DECK_OP_BITS + DECK_COND_BITS + 7 + DECK_INT_REGS                  \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckmapinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
 #if (DECK_OP_BITS + DECK_COND_BITS + 2 * DECK_GEN_REG_BITS + 1 \
-     + DECK_PORT_BITS + 4 != DECK_INST_BITS)
-#   error sizeof(struct deckioinst) != DECK_INST_BITS <deck/inst.h>
+     + DECK_PORT_BITS + 4                                      \
+     != DECK_INST_BITS)
+#   error sizeof(struct deckioinst) != DECK_INST_BITS in <deck/inst.h>
 #endif
 
 /* check structure sizes in <deck/mmu.h> */
 
-#if (DECK_PAGE_ADR_BITS + DECK_MTR_NPG_BITS + DECK_MTR_TYPE_BITS        \
+#if (DECK_PAGE_ADR_BITS + DECK_MTR_TYPE_BITS + 7                        \
      != DECK_REG_SIZE)
-#   error sizeof(struct deckmtr) != DECK_REG_SIZE in <deck/mmu.h>
+#   errof struct deckmtr header != DECK_REG_SIZE in <deck/mmu.h>
+#endif
+#if (DECK_PAGE_ADR_BITS + DECK_PAGE_PERM_BITS                           \
+     != DECK_REG_SIZE)
+#   errof struct deckmtr footer != DECK_REG_SIZE in <deck/mmu.h>
+#endif
+#if (DECK_PAGE_ADR_BITS + DECK_MTR_TYPE_BITS + 7                        \
+     + DECK_PAGE_ADR_BITS + DECK_PAGE_PERM_BITS                         \
+     != 2 * DECK_REG_SIZE)
+#   error sizeof(struct deckmtr) != DECK_REG_SIZE in in <deck/mmu.h>
 #endif
 
 /* check structure sizes in <deck/fpu.h> */
 
-#if (DECK_FLOAT_MANT_BITS + DECK_FLOAT_EXP_BITS != DECK_FLOAT_BITS)
+#if (DECK_FLOAT_MANT_BITS + DECK_FLOAT_EXP_BITS                         \
+     != DECK_FLOAT_BITS)
 #   errof sizeof(struct deckfloat) != DECK_FLOAT_BITS in <deck/fpu.h>
 #endif
 
-#if (DECK_DOUBLE_MANT_BITS + DECK_DOUBLE_EXP_BITS != DECK_DOUBLE_BITS)
+#if (DECK_DOUBLE_MANT_BITS + DECK_DOUBLE_EXP_BITS                       \
+     != DECK_DOUBLE_BITS)
 #   errof sizeof(struct deckdouble) != DECK_DOUBLE_BITS in <deck/fpu.h>
 #endif
 
