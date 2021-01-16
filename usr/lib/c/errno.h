3d44690aa1bd22925652d4f117b761a05459d8c7
Tuomo Venäläinen <tuomoster@gmail.com>
2021-01-16 13:06:27 +0200

    some changes

--- usr/lib/c/errno.h
+++ usr/lib/c/errno.h
@@ -3,5 +3,13 @@
 
 #include <api/errno.h>
 
+const char                     *const sys_errlist[];
+int                             sys_nerr;
+int                             errno;       /* Not really declared this way; see errno(3) */
+
+#if defined(__zen__)
+#   include <sys/zen/errno.h>
+#endif
+
 #endof /* ERRNO_H */
 
--- usr/lib/mach/v0/mem.h
+++ usr/lib/mach/v0/mem.h
@@ -1,15 +1,40 @@
 #ifndef MACH_V0_MEM_H
 #define MACH_V0_MEM_H
 
-#define MEM_EXEC      (1 << 0)
-#define MEM_WRITE     (1 << 1)
-#define MEM_READ      (1 << 2)
-#define MEM_WIRE      (1 << 3)
+/* SUPPORT FOR V0 ARCHITECTURE */
 
-#define MEM_PRESENT   (1 << 4)
-#define MEM_DIRTY     (1 << 5)
-#define MEM_SYS       (1 << 6)
-#define MEM_GROW_DOWN (1 << 7)
-#define MEM_DYNAMIC   (1 << 8)
+/* page-descriptor flags */
+#define V0_MEM_EXEC     (1 << 0)    // execute permission
+#define V0_MEM_WRITE    (1 << 1)    // write permission
+#define V0_MEM_READ     (1 << 2)    // read permission
+#define V0_MEM_PRESENT  (1 << 3)    // page is present in RAM
+#define V0_MEM_DIRTY    (1 << 4)    // page has been written into
+#define V0_MEM_WIRE     (1 << 5)    // page is mapped to RAM permanently
+#define V0_MEM_SYSTEM   (1 << 6)    // system-page (as opposed to user-mode)
+#define V0_MEM_NONTEMP  (1 << 7)    // non-temporary [uncached] hint
+/* page-allocator flags */
+#define V0_SEG_DYNAMIC  (1 << 8)    // dynamically-allocated segment
+#define V0_SEG_GROWDOWN (1 << 9)    // grow-down bit for stack segments
+#define V0_SEG_ZERO     (1 << 10)   // zero on allocation
+/* flags for process segments */
+#define V0_SEG_TRAP     V0_MEM_READ
+#define V0_SEG_RODATA   V0_MEM_READ
+#define V0_SEG_TEXT     (V0_MEM_READ | V0_MEM_EXEC)
+#define V0_SEG_DATA     (V0_MEM_READ | V0_MEM_WRITE)
+#define V0_SEG_BSS      (V0_MEM_DATA | V0_MEM_ZERO | V0_SEG_DYNAMIC)
+#define V0_SEG_STACK    (V0_MEM_READ | V0_MEM_WRITE | V0_SEG_GROWDOWN)
+#define V0_SEG_COMM     V0_MEM_RODATA
+#define V0_SEG_DEBUG    V0_MEM_RODATA
+#define V0_SYS_SEG(seg) ((seg) | V0_MEM_SYSTEM)
+/* segment IDs */
+#define V0_TRAP_SEG     0
+#define V0_RODATA_SEG   1
+#define V0_TEXT_SEG     2
+#define V0_DATA_SEG     3
+#define V0_BSS_SEG      4
+#define V0_COMM_SEG     5
+#define V0_DEBUG_SEG    6
+#define V0_CUSTOM_SEG   7
+#define V0_SEGS         8
 
 #endif /* MACH_V0_MEM_H */
